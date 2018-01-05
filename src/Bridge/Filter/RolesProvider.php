9aaa0a14e481b54d3e418fe17dcb1b4ba4fc2e0b
Ivan Barlog <ivan.barlog@everlution.sk>
2018-01-06 00:23:29 +0100

    move RolesProvider to bundle

---
+++ src/Bridge/Filter/RolesProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Everlution\NavigationBundle\Bridge\Filter;
+
+use Everlution\Navigation\Filter\RolesProviderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+
+/**
+ * Class RolesProvider.
+ *
+ * @author Ivan Barlog <ivan.barlog@everlution.sk>
+ */
+class RolesProvider implements RolesProviderInterface
+{
+    /** @var TokenStorage */
+    private $tokenStorage;
+
+    public function __construct(TokenStorage $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public function getRoles(): array
+    {
+        try {
+            return array_merge(
+                $this->tokenStorage->getToken()->getUser()->getRoles(),
+                ['IS_AUTHENTICATED_FULLY']
+            );
+        } catch (\Exception $exception) {
+            return ['IS_AUTHENTICATED_ANONYMOUSLY'];
+        }
+    }
+}
